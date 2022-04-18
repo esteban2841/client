041c91a175cfa590fce8691b83f474e0f9f88211
Esteban Puentes Roldan <rapuentes@uan.edu.co>
2022-04-18 09:22:18 -0500

    added styles to landing

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -6,10 +6,10 @@ export default function LandingPage (){
 
 
     return (
-        <div>
-            <h1>this is the landing page</h1>
+        <div className="landing">
+            <h1>Welcome to the pokemons' world</h1>
             <img src="https://p4.wallpaperbetter.com/wallpaper/283/550/399/pokemon-1920x1080-anime-pokemon-hd-art-wallpaper-preview.jpg" alt="" />
-            <button><Link to={"/home"}>poke</Link></button>
+            <button><Link to={"/home"}>Let's start</Link></button>
         </div>
     )
 
