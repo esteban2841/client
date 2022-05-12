ab1415ff8486cacf9d9e7a52866d6fd8d24c27a4
Esteban Puentes Roldan <rapuentes@uan.edu.co>
2022-05-12 09:39:03 -0500

    creating Assets folder including picture to be a back image

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -8,7 +8,6 @@ export default function LandingPage (){
     return (
         <div className="landing">
             <h1>Welcome to the pokemons' world</h1>
-            <img src="https://p4.wallpaperbetter.com/wallpaper/283/550/399/pokemon-1920x1080-anime-pokemon-hd-art-wallpaper-preview.jpg" alt="" />
             <button><Link to={"/home"}>Let's start</Link></button>
         </div>
     )
