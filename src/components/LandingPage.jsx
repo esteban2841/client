fd0e9f8c9bf1a68542902375862d318c143e3090
Esteban Puentes Roldan <rapuentes@uan.edu.co>
2022-05-12 10:00:01 -0500

    trying with image

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components"
+import landingImage from "../../src/Assets/LandingImage.png"
+
+
+const LandingContainer = styled.div`
+
+.landing{
+
+    background-image: landingImage;
+}
+`
 
 export default function LandingPage (){
 
 
 
     return (
-        <div className="landing">
+        <LandingContainer className="landing">
             <h1>Welcome to the pokemons' world</h1>
             <button><Link to={"/home"}>Let's start</Link></button>
-        </div>
+        </LandingContainer>
     )
 
 
