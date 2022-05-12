51036354290635f24e3a0d3644461d124d331316
Esteban Puentes Roldan <rapuentes@uan.edu.co>
2022-05-12 13:31:08 -0500

    modified landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components"
 import landingImage from "../../src/Assets/PokemonsLanding.png"
-import pokeball from "../../src/Assets/pokeBall.png"
+import pokeball from "../../src/images/pokeball.png"
 
 // const landingImage = require("../Assets/LandingImage.png")
 
@@ -15,29 +15,62 @@ const LandingContainer = styled.div`
     background-position: center;
     background-size: cover;
     
+    
     button{
-        border-radius: 50px;
-        color: white ;
-        width: 100px;
-        height: 100px;
+        position: absolute;
+        overflow: visible;
+        z-index: 1000;
+        top: 50%;
+        left: 50.25%;
+        border-radius: 50%;
+        background-color: transparent;
+        width: 75px;
+        height: 75px;
         cursor: pointer;
+        font-size: 14px;
+        border-style: none;
     }
+    
+    img{
+        position: absolute;
+        overflow: visible;
+        top: 43%;
+        left: 47%;
+        width: 200px;
+        cursor: pointer;
+        
+    }
+    
+    .image-container:hover{
+        button{
+            top: 50%;
+            left: 50.25%;
+            background-color: #e6e9ed;
+            box-shadow: 0 0 10px, 0 0 50px, 0 0 100px #ffffffe8;
+            z-index: 1001;
+            border: 15px solid #434a54;
+            width: 75px;
+            height: 75px;
+        }
+    }
+    
+    
 `
 
 export default function LandingPage (){
     const navigate = useNavigate()
-    const [isOpen, setIsOpen] = useState(false)
+    // const [isOpen, setIsOpen] = useState(false)
     function navigateHome (){
         navigate("/home")
     }
-    console.log(isOpen)
+    // console.log(isOpen)
     return (
         <LandingContainer className="landing">
-            <h1>Welcome to the pokemons' world</h1>
-            <img src={isOpen? pokeball : pokeball } alt=""/>
-            <button onClick={()=>{
-                setIsOpen(true)
-            }}>ENTER¿</button>
+            {/* <h1>Welcome to the pokemons' world</h1> */}
+            <div className="image-container" onClick={navigateHome}>
+                <img src={pokeball } alt="No aparece"/>
+                <button ></button>
+            </div>
         </LandingContainer>
     )
 
